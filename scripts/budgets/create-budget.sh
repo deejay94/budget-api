188948a9e9ee6f6c9192b9cd8a0f7aaa20bc9049
deejay94 <dahjoyner12@gmail.com>
2017-11-01 12:03:37 -0400

    Curl Scripts
    Add curl script files for create, get, update, and delete actions for
    budgets resource

---
+++ scripts/budgets/create-budget.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/budgets"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "budget": {
+      "name": "'"${NAME}"'",
+      "goal": "'"${GOAL}"'"
+    }
+  }'
+
+echo
