9fe1d42b84cb816a3ad014f718144603a24b33d2
deejay94 <dahjoyner12@gmail.com>
2017-11-01 10:39:24 -0400

    Generate scaffold files with resource 'budget'
    -Changed budgets controller to protectedread controller so users will
    only be able to see their content
    -Add budget belongs to user
    -Add user has many budgets

---
+++ app/controllers/budgets_controller.rb
@@ -0,0 +1,51 @@
+class BudgetsController < ProtectedController
+  before_action :set_budget, only: [:update, :destroy]
+
+  # GET /budgets
+  def index
+    @budgets = current_user.budgets
+
+    render json: @budgets
+  end
+
+  # GET /budgets/1
+  def show
+    render json: Budget.find(params[:id])
+  end
+
+  # POST /budgets
+  def create
+    @budget = current_user.budgets.build(budget_params)
+
+    if @budget.save
+      render json: @budget, status: :created
+    else
+      render json: @budget.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /budgets/1
+  def update
+    if @budget.update(budget_params)
+      head :no_content
+    else
+      render json: @budget.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /budgets/1
+  def destroy
+    @budget.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_budget
+      @budget = current_user.budgets.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def budget_params
+      params.require(:budget).permit(:name, :goal)
+    end
+end
